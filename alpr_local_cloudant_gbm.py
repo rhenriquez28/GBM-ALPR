44b9b5851d4bfacb057516675851b540f033b880
Kevin Kantule <kevin.kantule@gmail.com>
2019-02-06 17:05:53 -0500

    enlace del local con Cloudant

--- alpr_local_cloudant_gbm.py
+++ alpr_local_cloudant_gbm.py
@@ -2,6 +2,55 @@ from openalpr import Alpr
 import cv2
 import sys
 
+#Conectar con una instancia de servicio de IBM Cloudant en IBM Cloud
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
+
+#aquí se ponen las credenciales de servicio de la DB
+serviceUsername = "user_name"
+servicePassword = "password"
+serviceURL = "URL"
+
+#establecer una conexión con la instancia de servicio
+client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
+client.connect()
+
+#crear una base de datos dentro de la instancia de servicio
+databaseName = "databasedemo"
+#myDatabaseDemo = client.create_database(databaseName)
+#if myDatabaseDemo.exists():
+#    print (""{0}" successfully created.\n".format(databaseName))
+
+#Llamada directa a un punto final de API de IBM Cloudant
+def comparePlate(comPlate):
+    sospechoso = comPlate
+    print("sospechoso IN: "+sospechoso)
+
+    end_point = "{0}/{1}".format(serviceURL, databaseName + "/_all_docs")
+    params = {"include_docs": "true"}
+    
+    response = client.r_session.get(end_point, params=params)
+    response = response.json()
+
+    #x = (response["rows"][0]["doc"]["matricula"])
+    #print("sospechoso DB "+x)
+    #if (x==sospechoso):
+    #    print("La matricula "+sospechoso+" es sospechoso")
+    #else:
+    #    print("no es sospechoso") 
+    i =0
+    for x in response: 
+        
+        x = (response["rows"][i]["doc"]["matricula"])
+        print("sospechoso DB "+x)
+        if (x==sospechoso):
+            print("La matricula "+sospechoso+" es sospechoso")
+        else:
+            print("no es sospechoso")
+        i+=1 
+    return sospechoso
+
 # Global variables
 #Aqui se especifica donde esta el video
 VIDEO_PATH = "/home/roy/Desktop/noche.mp4"
